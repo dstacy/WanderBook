15e7ba65834c9142571f9ebbbdcbeef43dba2e47
Darcy Stacy <89650606+dstacy@users.noreply.github.com>
2023-09-18 21:55:08 -0400

    Update Auth.js
    Updated wording for password confirmation.

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -70,7 +70,7 @@ const SignUp = () => {
             )}
             <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
             <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
-            { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
+            { isSignup && <Input name="confirmPassword" label="Confirm Password" handleChange={handleChange} type="password" /> }
           </Grid>
           <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
             { isSignup ? 'Sign Up' : 'Sign In' }
