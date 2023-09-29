4ebcdd3dfcba7f3058e17235ab149f57515c8996
Darcy Stacy <89650606+dstacy@users.noreply.github.com>
2023-09-29 14:24:00 -0400

    Updated MongoDB connection credentials.

--- server/index.js
+++ server/index.js
@@ -16,11 +16,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+// Note to self: move creds to environmental before deployment
+const CONNECTION_URL = 'mongodb+srv://441Slayer:CSCI441Slayer@cluster0.avwlc2m.mongodb.net/';
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
-
-mongoose.set('useFindAndModify', false);
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)));
+  //.catch((error) => console.log(`${error} did not connect`)); // depreciated
