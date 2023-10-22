9d850f0e3a4c7b43a4a2a707422027b1a9b2a160
Darcy Stacy <89650606+dstacy@users.noreply.github.com>
2023-10-22 17:29:32 -0400

    Created buttons and routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 import CreatorOrTag from './components/CreatorOrTag/CreatorOrTag';
+import Search from './components/Search/Search';
+import Lists from './components/Lists/Lists';
+import ListDetails from './components/ListDetails/Listdetails';
 
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
@@ -18,10 +21,12 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={() => <Redirect to="/posts" />} />
           <Route path="/posts" exact component={Home} />
-          <Route path="/posts/search" exact component={Home} />
+          <Route path="/search" exact component={Search} />
+          <Route path="/lists" exact component={Lists} />
           <Route path="/posts/:id" exact component={PostDetails} />
+          <Route path="/lists/:id" exact component={ListDetails} />
           <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
-          <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
+          <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} /> 
         </Switch>
       </Container>
     </BrowserRouter>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -45,7 +45,10 @@ const Navbar = () => {
           <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+            <Button className={classes.buttons} component={Link} to="/posts" variant="contained" color="primary">Posts</Button>
+            <Button className={classes.buttons} component={Link} to="/lists" variant="contained" color="primary">Lists</Button>
+            <Button className={classes.buttons} component={Link} to="/search" variant="contained" color="primary">Search</Button>
+            <Button variant="contained" className={classes.buttons} color="secondary" onClick={logout}>Logout</Button>
           </div>
         ) : (
           <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
