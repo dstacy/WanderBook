d4eb43189511d91449da11aeaf876cb38fc609d3
Darcy Stacy <89650606+dstacy@users.noreply.github.com>
2023-10-24 09:42:24 -0400

    Added additional search filters to form - UI only

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Grow, Grid, AppBar, TextField, Button, Paper } from '@material-ui/core';
+import { Container, Grow, Grid, AppBar, TextField, Button, Paper, FormControlLabel, Checkbox } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
 import ChipInput from 'material-ui-chip-input';
@@ -20,6 +20,13 @@ const Search = () => {
   const searchQuery = query.get('searchQuery');
 
   const [currentId, setCurrentId] = useState(0);
+
+  const [site, setSite] = useState('');
+  const [state, setState] = useState('');
+  const [ampsSelected, setAmpsSelected] = useState(false);
+  const [waterSelected, setWaterSelected] = useState(false);
+  const [petsSelected, setPetsSelected] = useState(false);
+  const [sewerSelected, setSewerSelected] = useState(false);
   const dispatch = useDispatch();
 
   const [search, setSearch] = useState('');
@@ -48,24 +55,111 @@ const Search = () => {
   return (
     <Grow in>
       <Container maxWidth="xl">
-        <Grid container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+        <Grid
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+          className={classes.gridContainer}
+        >
           <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <AppBar className={classes.appBarSearch} position="static" color="inherit">
-              <TextField onKeyDown={handleKeyPress} name="search" variant="outlined" label="Search Memories" fullWidth value={search} onChange={(e) => setSearch(e.target.value)} />
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              <TextField
+                onKeyDown={handleKeyPress}
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                fullWidth
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
               <ChipInput
-                style={{ margin: '10px 0' }}
+                style={{ margin: "10px 0" }}
                 value={tags}
                 onAdd={(chip) => handleAddChip(chip)}
                 onDelete={(chip) => handleDeleteChip(chip)}
                 label="Search Tags"
                 variant="outlined"
               />
-              <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
+              <TextField
+                name="site"
+                variant="outlined"
+                label="Site #"
+                fullWidth
+                value={site}
+                onChange={(e) => setSite(e.target.value)}
+              />
+              <TextField
+                style={{ margin: "10px 0" }}
+                name="state"
+                variant="outlined"
+                label="State ex: KY"
+                fullWidth
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={ampsSelected}
+                    onChange={(e) => setAmpsSelected(e.target.checked)}
+                    name="ampsSelected"
+                    color="primary"
+                  />
+                }
+                label="Amps"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={waterSelected}
+                    onChange={(e) => setWaterSelected(e.target.checked)}
+                    name="waterSelected"
+                    color="primary"
+                  />
+                }
+                label="Water"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={petsSelected}
+                    onChange={(e) => setPetsSelected(e.target.checked)}
+                    name="petsSelected"
+                    color="primary"
+                  />
+                }
+                label="Pets"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={sewerSelected}
+                    onChange={(e) => setSewerSelected(e.target.checked)}
+                    name="sewerSelected"
+                    color="primary"
+                  />
+                }
+                label="Sewer"
+              />
+
+              <Button
+                onClick={searchPost}
+                className={classes.searchButton}
+                variant="contained"
+                color="primary"
+              >
+                Search
+              </Button>
             </AppBar>
-            {(!searchQuery && !tags.length) && (
+            {!searchQuery && !tags.length && (
               <Paper className={classes.pagination} elevation={6}>
                 <Pagination page={page} />
               </Paper>
