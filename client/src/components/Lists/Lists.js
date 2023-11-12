a0b91c5adcdc22f0c3ac2ebc3da6fd73594b9ced
Justin Snowden <84579965+Snowman106@users.noreply.github.com>
2023-11-12 09:32:49 -0600

    Updated List Display
    The display now only lists the list for the logged in user.

--- client/src/components/Lists/Lists.js
+++ client/src/components/Lists/Lists.js
@@ -7,14 +7,17 @@ import useStyles from './styles';
 
 const Lists = ({ setCurrentId }) => {
   const { lists, isLoading } = useSelector((state) => state.lists);
+  const currentUser = JSON.parse(localStorage.getItem('profile')) || { result: { name: null } };
   const classes = useStyles();
 
   if (!lists.length && !isLoading) return 'No lists';
 
+  const userLists = lists.filter((list) => list.creator === currentUser.result.name);
+
   return (
     isLoading ? <CircularProgress /> : (
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-        {lists?.map((list) => (
+        {userLists.map((list) => (
           <Grid key={list._id} item xs={12} sm={12} md={6} lg={3}>
             <List list={list} setCurrentId={setCurrentId} />
           </Grid>
