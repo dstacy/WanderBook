b51f53b415684e75f8d8964cd6c60a1305250a49
Justin Snowden <84579965+Snowman106@users.noreply.github.com>
2023-11-12 20:17:49 -0600

    Public Lists
    Added a checkbox to be able to view lists that were made public.
    
    Also removed code that seemed to be unnecessary.

--- client/src/components/Lists/Lists.js
+++ client/src/components/Lists/Lists.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Grid, CircularProgress } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Grid, CircularProgress, Checkbox, FormControlLabel } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import List from './List/List';
@@ -10,23 +10,35 @@ const Lists = ({ setCurrentId }) => {
   const currentUser = JSON.parse(localStorage.getItem('profile')) || { result: { name: null } };
   const classes = useStyles();
 
-  useEffect(() => {
-    
-  }, [lists]);
+  const [showPublicLists, setShowPublicLists] = useState(false);
 
   if (!lists.length && !isLoading) return 'No lists';
 
-  const userLists = lists.filter((list) => list.creator === currentUser.result.name).sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  // Create two lists to view public Lists and user's Lists
+  const publicLists = lists.filter((list) => list.isPublic);
+  const userLists = lists.filter((list) => list.creator === currentUser.result.name);
 
+  // Combine lists based on the showPublicLists value
+  const displayedLists = showPublicLists ? publicLists : userLists;
+
+  // Sort displayed lists by most recent to least recent
+  const sortedLists = displayedLists.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  
   return (
-    isLoading ? <CircularProgress /> : (
+    isLoading ? ( <CircularProgress /> ) : (
+      <div>
+        <FormControlLabel
+          control={<Checkbox checked={showPublicLists} onChange={() => setShowPublicLists(!showPublicLists)} />}
+          label="Show Public Lists"
+        />
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-        {userLists.map((list) => (
+        {sortedLists.map((list) => (
           <Grid key={list._id} item xs={12} sm={12} md={6} lg={3}>
             <List list={list} setCurrentId={setCurrentId} />
           </Grid>
         ))}
       </Grid>
+      </div>
     )
   );
 };
